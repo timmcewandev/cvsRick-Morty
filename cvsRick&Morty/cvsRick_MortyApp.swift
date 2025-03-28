614df12ce756e2fa900b9a7b1ecbf0e8de1fafeb
Tim McEwan <timmcewandev@gmail.com>
2025-03-28 09:10:51 -0600

    Initial Commit

---
+++ cvsRick&Morty/cvsRick_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  cvsRick_MortyApp.swift
+//  cvsRick&Morty
+//
+//  Created by Tim McEwan on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct cvsRick_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
